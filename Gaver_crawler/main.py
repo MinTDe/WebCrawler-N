4382aea2abf0e016d58eed149e3a82786702d607
mintde <yc424k@gmail.com>
2019-02-13 18:52:10 +0900

    version delta

--- Gaver_crawler/main.py
+++ Gaver_crawler/main.py
@@ -1,20 +1,26 @@
 # version
 # version - alpha -> 검색어 crawler 기능 추가
-# version - beta -> 검색어 csv로 저장 기능 추가
-# version - gamma -> 검색어가 다를시 인식 기능 추가
+# version - beta -> 검색어 csv로 저장
+# version - gamma -> 검색어가 다를시 인식
+# version - delta -> 현재 시간 출력
 
 # main.py
+
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from pandas import DataFrame
 from time import sleep
+from datetime import datetime
 
 df_ranking = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 ori_df_topic =[None] * 20
 df_topic = [None] * 20
 
-while(True):
+#while(10):
+for i in range(4):
+    time = datetime.now()
+    print(time)
     html = requests.get('https://www.naver.com').text
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -34,4 +40,5 @@ while(True):
     df = pd.DataFrame(df_basic)
     print(df)
     sleep(3)
+
     #df.to_csv("D:\Coding\WebCrawler-N\Gaver_crawler\data\data.csv", index = False)
