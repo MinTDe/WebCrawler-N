a99a6d97f419cae4bd2aceb2c5dc878a0aa06407
mintde <yc424k@gmail.com>
2019-02-15 03:06:30 +0900

    version 1.0.0

--- Gaver_crawler/main.py
+++ Gaver_crawler/main.py
@@ -4,7 +4,8 @@
 # version - gamma -> 검색어가 다를시 인식
 # version - delta -> 현재 시간 출력
 # version - epsilon -> 현재시각 파일이름 저장
-# version - zeta -> 기본적인 기능 모두 구현
+# version - zeta -> 기본적인 기능 모두 구현 -> version 1.0.0
+# version 2.0 조건 django 서버와 연동
 
 # main.py
 
