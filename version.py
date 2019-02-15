ea4fc2af7c92524e44feb64431505e4ac87b566b
mintde <yc424k@gmail.com>
2019-02-15 22:33:06 +0900

    version 1.0.0.0

---
+++ version.py
@@ -0,0 +1,8 @@
+# version
+# version - alpha -> 검색어 crawler 기능 추가
+# version - beta -> 검색어 csv로 저장
+# version - gamma -> 검색어가 다를시 인식
+# version - delta -> 현재 시간 출력
+# version - epsilon -> 현재시각 파일이름 저장
+# version - zeta -> 기본적인 기능 모두 구현 -> version 1.0.0
+# version 2.0 조건 django 서버와 연동
